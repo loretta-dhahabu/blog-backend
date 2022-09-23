6422694f8cb6bd0df30cfe84bf9df691a162e42f
loretta dhahabu <loreydjefwa@gmail.com>
2022-09-23 16:33:04 +0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
 puts ":seedling: Seeding spices..."
 # Seed your database here
 # seed through articles table
-15.times do Article.create(
+10.times do Article.create(
     title: Faker::Book.title,
     image_url: Faker::LoremFlickr.image,
     content: Faker::Lorem.paragraph
     )
 end
 # seed through users table
-15.times do User.create(
+10.times do User.create(
     name: Faker::Name.name,
     email: Faker::Internet.email,
     password: Faker::Internet.password
     )
 end
 # seed through testimonials table
-15.times do Testimonial.create(
+10.times do Testimonial.create(
     testimony: Faker::Lorem.paragraph,
     article_id: Article.all.sample.id,
     user_id: User.all.sample.id
